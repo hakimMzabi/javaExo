fc3293cc54f15a08f4d7606c14954492510537fc
hakimmzabi <hakim.mzabi@beeops.net>
2019-02-10 14:41:09 +0100

    exo 2:preambule

---
+++ exo2Java/src/exo1/Employee.java
@@ -0,0 +1,12 @@
+package exo1;
+
+import java.util.Date;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private Date date;
+    private Service service;
+
+}
---
+++ exo2Java/src/exo1/Service.java
@@ -0,0 +1,10 @@
+package exo1;
+
+import java.util.List;
+
+public class Service {
+
+    private String name;
+    private String function;
+    private List<Employee> list;
+}
